42d31c4b36a14df52dec18ac5cee58f85398a64f
Maxi Paz <maximiliano.paz@kavak.com>
2022-08-19 10:42:27 -0300

    add empty first test

---
+++ test/calculator.test.js
@@ -0,0 +1,16 @@
+
+describe('Calculator ', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  // Given
+  const equation = '0 + 5 / 1';
+  it(`Given a equation ${equation} , when try to resolve, then check calls and return value`, () => {
+    // When
+   
+    // Then
+    
+    
+  });
+
+});
